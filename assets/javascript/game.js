9635d536521e2e0be2d61a8e06e661fb1dd88403
Riley Brown <brown.riley.c@gmail.com>
2018-11-28 10:26:35 -0500

    Added the on click to move the character cards.  Made heroSelected to stop from reselecting.  Made objects for characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,39 @@
+var cloud = {
+    name: "Cloud",
+    health: 175,
+    attack: 7,
+    power: 7,
+    counter: 15
+};
+var cid = {
+    name: "Cid",
+    health: 150,
+    attack: 10,
+    power: 10,
+    counter: 25
+};
+var kefka = {
+    name: "Kefka",
+    health: 225,
+    attack: 5,
+    power: 5,
+    counter: 10
+};
+var vivi = {
+    name: "Vivi",
+    health: 125,
+    attack: 12,
+    power: 12,
+    counter: 5
+};
+
 var characterArray = ["Cloud", "Cid", "Kefka", "Vivi"];
 
-var runCounter = 0;
+var heroSelected = false;
 
 var characterImageArray = ["src='./assets/images/cloud.png'>", "src='./assets/images/cid.png'>", "src='./assets/images/kefka.png'>", "src='./assets/images/Vivi.png'>"]
-console.log(characterArray[0]);
 
 function createCharacterCard(num) {
-    runCounter++;
-    console.log("I ran " + runCounter);
     var characterDiv = $('<div>');
     characterDiv.addClass(characterArray[num]);
     characterDiv.appendTo(".characterRow");
@@ -24,10 +50,53 @@ function createCharacterCard(num) {
     characterHealth.text("HP");
 }
 
+
+
 $(document).ready(function() {
     
     for (var i = 0; i < characterArray.length; i++) {
         console.log(characterArray.length);
         createCharacterCard(i);
     }
+
+    $('.Cloud').click(function(){
+        if (heroSelected === false) {
+        $('.Cloud').detach().appendTo('.heroRow');
+        $('.Cid').detach().appendTo('.rivalRow');
+        $('.Kefka').detach().appendTo('.rivalRow');
+        $('.Vivi').detach().appendTo('.rivalRow');
+        heroSelected = true;
+        };
+        console.log(heroSelected);
+    });
+
+    $('.Cid').click(function(){
+        if (heroSelected === false) {
+        $('.Cloud').detach().appendTo('.rivalRow');
+        $('.Cid').detach().appendTo('.HeroRow');
+        $('.Kefka').detach().appendTo('.rivalRow');
+        $('.Vivi').detach().appendTo('.rivalRow');
+        heroSelected = true;
+        };
+    });
+
+    $('.Kefka').click(function(){
+        if (heroSelected === false) {
+        $('.Cloud').detach().appendTo('.rivalRow');
+        $('.Cid').detach().appendTo('.rivalRow');
+        $('.Kefka').detach().appendTo('.heroRow');
+        $('.Vivi').detach().appendTo('.rivalRow');
+        heroSelected = true;
+        };
+    });
+    
+    $('.Vivi').click(function(){
+        if (heroSelected === false) {
+        $('.Cloud').detach().appendTo('.rivalRow');
+        $('.Cid').detach().appendTo('.rivalRow');
+        $('.Kefka').detach().appendTo('.rivalRow');
+        $('.Vivi').detach().appendTo('.heroRow');
+        heroSelected = true;
+        };
+    });
 });
